60d87d1587fb16c30cfa7e4459a017dc358a9fb5
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2023-12-04 20:49:28 +0100

    correct answer day2

--- Day01.py
+++ Day01.py
@@ -1,22 +1,22 @@
-import csv;
-from csv import reader;
+import csv
+from csv import reader
 
 # Open the CSV file for reading
-with open(r'd:\link\to\file\\input-text.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+with open(r"d:\link\to\file\\input-text.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter=",", quotechar='"')
 
     # Initialize a list to store the sums of first and last digits
     sums = []
 
     for row in reader:
         # Initialize a variable to store the concatenated digits
-        concatenated_digits = ''
+        concatenated_digits = ""
         for i in row[0]:
-            print('Value of i: '+ i)
+            print("Value of i: " + i)
             if i.isdigit():
                 concatenated_digits += i
-                first_digit = (concatenated_digits[0])
-                last_digit = (concatenated_digits[-1])
+                first_digit = concatenated_digits[0]
+                last_digit = concatenated_digits[-1]
         row_num = first_digit + last_digit
         print(row_num)
         sums.append(int(row_num))
--- Day02.py
+++ Day02.py
@@ -1,62 +1,61 @@
-import csv;
-from csv import reader;
+possible_games = []
+impossible_games = []
 
-file_path = '/Users/ameliem/coding/portfolio/AdventOfCode/input/Input-Day02.csv'
-# Open the CSV file for reading
-with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+# file_path = "/Users/ameliem/coding/portfolio/AdventOfCode/input/Input-Day02.txt"
+file_path = "/Users/ameliem/coding/portfolio/AdventOfCode/input/Input-Day02.txt"
+with open(file_path) as input_file:
+    for line in input_file:
+        print(line)
 
-        
-# Initialize a list to store the sums of first and last digits
-    possible_games = []
-    impossible_games = []
-    game_id = 0
+        # Game 88: 2 blue, 4 red, 8 green; 4 blue, 7 red; 3 red, 10 green, 4 blue; 9 green, 3 blue, 5 red; 4 red, 6 blue, 3 green
+        game_name, game_result = line.strip().split(":")
+        print(game_name)
+        game_id = int(game_name.split()[1])
+        #_, game_id = game_name.split()
+        # _ underscore for variables we don't want to use / keep
+        # print(game_id)
+        # print(game_result)
+        cleaned_row = game_result.replace(";", ",")
 
-    for row in reader:
-        game_id += 1 
-        print(game_id)
-        for item in row:
-            #Clean the rows from the semicolons
-            cleaned_row = [item.replace(';',',')]
+        red_count = 0
+        green_count = 0
+        blue_count = 0
 
-            #Split game number from results
-            splitted_row = cleaned_row[0].split(':')
-            #print(splitted_row)
-            game_nr = splitted_row[0].strip()
-            #print(game_nr)
-            
-            # Initialize counters for red, green, and blue
-            
-            red_count = 0
-            green_count = 0
-            blue_count = 0
+        array_result_per_game = cleaned_row.split(", ")
+        print(array_result_per_game)
 
-            result_per_game = splitted_row[1].replace('"','').strip()
-            array_result_per_game = result_per_game.split(', ')
-            print(array_result_per_game)
-            
-            for result_item in array_result_per_game:    
-                if 'red' in result_item:
-                   red_count += int(result_item.replace(' red',''))
-                if 'green' in result_item:
-                   green_count += int(result_item.replace(' green',''))
-                if 'blue' in result_item:
-                   blue_count += int(result_item.replace(' blue',''))
-                    #print(red_count)
-            print(red_count, green_count, blue_count)
+        for result_item in array_result_per_game:
+            cube_count, cube_color = result_item.split()
+            # print(cube_count)
 
-            if red_count <= 12 and green_count <= 13 and blue_count <= 14:
-                possible_games.append(game_id)
-                print(game_id)
+            # takes the highest value for red, blue and green
+            if "red" == cube_color and red_count <= int(cube_count):
+                    red_count = int(cube_count)
+            if "green" == cube_color and green_count <= int(cube_count):
+                green_count = int(cube_count)
+            if "blue" == cube_color and blue_count <= int(cube_count):
+                blue_count = int(cube_count)
 
-            if red_count > 12 or green_count > 13 or blue_count > 14:
-                impossible_games.append(game_id)
-                print(game_id)
+        print(red_count, green_count, blue_count)
 
-    # Calculate the sum of possible game IDs
-    print(possible_games)
-    sum_possible_games = sum(possible_games)
-    print(sum_possible_games)
-    print(impossible_games)
-    sum_impossible_games = sum(impossible_games)
-    print(sum_impossible_games)
+        if red_count <= 12 and green_count <= 13 and blue_count <= 14:
+            possible_games.append(game_id)
+            print(game_id)
+
+        if red_count > 12 or green_count > 13 or blue_count > 14:
+            impossible_games.append(game_id)
+            print(game_id)
+
+print("possible games:")
+print(possible_games)
+print("impossible games:")
+print(impossible_games)
+
+sum_possible_games = sum(possible_games)
+print("sum_possible_games:", sum_possible_games)
+
+sum_impossible_games = sum(impossible_games)
+print("sum_impossible_games:", sum_impossible_games)
+
+# pip3 install black
+# pip3 install flake8
